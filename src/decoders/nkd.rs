f736a4530945f8a2808bf5196f3ef3149b967872
Marco Grassi <marco.gra@gmail.com>
2022-08-05 14:31:42 +0800

    correct formatting

--- src/decoders/nkd.rs
+++ src/decoders/nkd.rs
@@ -1,39 +1,45 @@
-use crate::decoders::*;
 use crate::decoders::basics::*;
+use crate::decoders::*;
 use std::f32::NAN;
 
 //#[derive(Debug, Clone)]
 pub struct NakedDecoder<'a> {
-  buffer: &'a [u8],
-  camera: Camera,
+    buffer: &'a [u8],
+    camera: Camera,
 }
 
 impl<'a> NakedDecoder<'a> {
-  pub fn new(buf: &'a [u8], cam: Camera, _rawloader: &'a RawLoader) -> NakedDecoder<'a> {
-    NakedDecoder {
-      buffer: buf,
-      camera: cam,
+    pub fn new(buf: &'a [u8], cam: Camera, _rawloader: &'a RawLoader) -> NakedDecoder<'a> {
+        NakedDecoder {
+            buffer: buf,
+            camera: cam,
+        }
     }
-  }
 }
 
 impl<'a> Decoder for NakedDecoder<'a> {
-  fn image(&self, dummy: bool) -> Result<RawImage,String> {
-    let width = self.camera.raw_width;
-    let height = self.camera.raw_height;
-    let size = self.camera.filesize;
-    let bits = size*8 / width / height;
+    fn image(&self, dummy: bool) -> Result<RawImage, String> {
+        let width = self.camera.raw_width;
+        let height = self.camera.raw_height;
+        let size = self.camera.filesize;
+        let bits = size * 8 / width / height;
 
-    let image = if self.camera.find_hint("12le_16bitaligned") {
-      decode_12le_16bitaligned(self.buffer, width, height, dummy)
-    } else {
-      match bits {
-        10 => decode_10le_lsb16(self.buffer, width, height, dummy),
-        12 => decode_12be_msb16(self.buffer, width, height, dummy),
-        _  => return Err(format!("Naked: Don't know about {} bps images", bits)),
-      }
-    };
+        let image = if self.camera.find_hint("12le_16bitaligned") {
+            decode_12le_16bitaligned(self.buffer, width, height, dummy)
+        } else {
+            match bits {
+                10 => decode_10le_lsb16(self.buffer, width, height, dummy),
+                12 => decode_12be_msb16(self.buffer, width, height, dummy),
+                _ => return Err(format!("Naked: Don't know about {} bps images", bits)),
+            }
+        };
 
-    ok_image(self.camera.clone(), width, height, [NAN,NAN,NAN,NAN], image)
-  }
+        ok_image(
+            self.camera.clone(),
+            width,
+            height,
+            [NAN, NAN, NAN, NAN],
+            image,
+        )
+    }
 }
